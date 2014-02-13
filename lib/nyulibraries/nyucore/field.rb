81993d12425b3e3f5e705eed7cea7b1f2ed44801
Scot Dalton <scotdalton@gmail.com>
2014-02-13 14:43:21 -0500

    Add the validate name feature

--- lib/nyulibraries/nyucore/field.rb
+++ lib/nyulibraries/nyucore/field.rb
@@ -1,11 +1,17 @@
 module Nyulibraries
   module NyuCore
     class Field
+      NAMES = [:identifier, :accessURL, :archiveURL, :title, :alternative,
+        :vernacular, :uniform, :type, :isPartOf, :collectionId, :creator,
+        :contributor, :ISBN, :ISSN, :subject, :genre, :classification, :date,
+        :modified, :publisher, :language, :coverage, :spatial, :temporal,
+        :extent, :format, :description, :abstract, :fullText, :rights]
+
       attr_accessor :name, :value
 
       # Initialize with a Hash of attributes
       #   e.g. Nyulibraries::NyuCore::Field.new(name: :subject:, value: "NYUCore vs. NyuCore")
-      # 
+      #
       # Valid keys are :name, :value.
       # Name can be passed either as a string or a symbol.
       # They will be converted to a symbol.
@@ -18,7 +24,11 @@ module Nyulibraries
       end
 
       def name=(name)
-        @name = name.to_sym
+        symbolized_name = name.to_sym
+        unless NAMES.include?(symbolized_name)
+          raise ArgumentError.new("Invalid name!")
+        end
+        @name = symbolized_name
       end
     end
   end
