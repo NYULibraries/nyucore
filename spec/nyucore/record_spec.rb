fd02a665c254a118f184d303d657cb21270ac887
Scot Dalton <scotdalton@gmail.com>
2014-02-14 12:48:30 -0500

    The fields :identifier and :title are required for NyuCore::Records at instantiation time.

--- spec/nyucore/record_spec.rb
+++ spec/nyucore/record_spec.rb
@@ -1,26 +1,63 @@
 require 'spec_helper'
 module NyuCore
   describe Record do
-    let(:attributes) do
+    let(:fields) do
       { 
-        subject: "NYUCore vs. NyuCore", 
-        "identifier" => 1234567890
+        "identifier" => 1234567890,
+        "title" => "NYUCore/NyuCore",
+        subject: "NYUCore vs. NyuCore"
       }
     end
-    subject(:record) { Record.new(attributes) }
-    it { should be_a(Record) }
+    context "when the required fields are present" do
+      subject(:record) { Record.new(fields) }
+      it { should be_a(Record) }
 
-    describe '#fields' do
-      subject(:fields) { record.fields }
-      it { should be_a(Hash) }
-      it("should have keys that are Symbols") do
-        fields.each_key do |key|
-          expect(key).to be_a(Symbol)
+      describe '#fields' do
+        subject { record.fields }
+        it { should be_a(Hash) }
+        it("should have keys that are Symbols") do
+          subject.each_key do |key|
+            expect(key).to be_a(Symbol)
+          end
+        end
+        it("should have values that are NyuCore::Fields") do
+          subject.each_value do |field|
+            expect(field).to be_a(Field)
+          end
         end
       end
-      it("should have values that are NyuCore::Fields") do
-        fields.each_value do |field|
-          expect(field).to be_a(Field)
+    end
+    context "when some required fields are missing" do
+      context "and it's the identifier that's missing" do
+        let(:invalid_fields) do
+          { 
+            "title" => "NYUCore/NyuCore",
+            subject: "NYUCore vs. NyuCore"
+          }
+        end
+        it("should raise an ArgumentError") do
+          expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
+        end
+      end
+      context "and it's the title that's missing" do
+        let(:invalid_fields) do
+          { 
+            "identifier" => 1234567890,
+            subject: "NYUCore vs. NyuCore"
+          }
+        end
+        it("should raise an ArgumentError") do
+          expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
+        end
+      end
+      context "and it's both the idenitifier and title that are missing" do
+        let(:invalid_fields) do
+          { 
+            subject: "NYUCore vs. NyuCore"
+          }
+        end
+        it("should raise an ArgumentError") do
+          expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
         end
       end
     end
