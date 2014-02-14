d96d8d080c0b6a436e03d26454e2075456e6872f
Scot Dalton <scotdalton@gmail.com>
2014-02-14 14:38:43 -0500

    Add the beginnings of logic for parsing OAIPMH XML and the associated specs

--- lib/nyucore/readers/oaipmh_reader.rb
+++ lib/nyucore/readers/oaipmh_reader.rb
@@ -1,6 +1,42 @@
 module NyuCore
   module Readers
+    require 'multi_xml'
     class OAIPMHReader
+      def initialize(input)
+        @input = input
+        unless File.exists?(input)
+          raise ArgumentError.new("File doesn't exist: #{input}")
+        end
+      end
+
+      def to_ary
+        @records = []
+        # Parse the files into a hash
+        xml = MultiXml.parse(input_file.read)
+        oaipmh_records = xml["OAI_PMH"]["ListRecords"]["record"]
+        unless oaipmh_records.nil?
+          oaipmh_records.each do |oaipmh_record|
+            oaipmh_header = oaipmh_record["header"]
+            identifier = identifier(oaipmh_header) if oaipmh_header
+            oaipmh_metadata = oaipmh_record["metadata"]
+          end
+        end
+        @records
+      end
+
+      def identifier(oaipmh_header)
+        # Clean up the identifiers
+        # Remove http://
+        # Replace dots(.), slashes(/) and double backslashes(\\) 
+        # with dashes(-)
+        oaipmh_header["identifier"].gsub('http://', '').gsub(/(.|\\|\/)/, '-')
+      end
+      private :identifier
+
+      def input_file
+        File.new(@input)
+      end
+      private :input_file
     end
   end
 end
--- spec/nyucore/readers/oaipmh_reader_spec.rb
+++ spec/nyucore/readers/oaipmh_reader_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper'
 module NyuCore
   module Readers
     describe OAIPMHReader do
+      let(:support_directory) { File.expand_path('../../', __FILE__) + '/support' }
       context "when it's instaniated with a valid filename" do
-        let(:support_directory) { File.expand_path('../../', __FILE__) + '/support' }
         let(:valid_oai_pmh_filename) { "#{support_directory}/readers/oaipmh.xml" }
-        subject(:reader) { binding.pry; OAIPMHReader.new(valid_oai_pmh_filename) }
+        subject(:reader) { OAIPMHReader.new(valid_oai_pmh_filename) }
         it { should be_a(OAIPMHReader) }
         describe '#to_ary' do
-          subject { reader.to_ary }
-          it { should be_a(::NyuCore::Record) }
+          subject(:records) { reader.to_ary }
+          it { should be_a(Array) }
+          it { should_not be_empty }
+          it("should have 3 elements") do
+            expect(records.size).to be(3)
+          end
+          it("should have NyuCore::Records as elements") do
+            records.each do |record|
+              expect(record).to be_an(::NyuCore::Record)
+            end
+          end
+        end
+      end
+      context "when it's instaniated with a invalid filename" do
+        let(:invalid_oai_pmh_filename) { "#{support_directory}/readers/invalid.xml" }
+        it("should raise an ArgumentError") do
+          expect { OAIPMHReader.new(invalid_oai_pmh_filename) }.to raise_error(ArgumentError)
         end
       end
     end
