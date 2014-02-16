a1a58c30c4128d272fc6cab7f21cdb0df4ef8a28
Scot Dalton <scotdalton@gmail.com>
2014-02-15 22:14:03 -0500

    Records can take fields with Arrays of Strings or just Strings as inputs, unless they are specifically "identity fields" in which case they can only take a String.

--- lib/nyucore/record.rb
+++ lib/nyucore/record.rb
@@ -1,20 +1,47 @@
 module NyuCore
   class Record
     REQUIRED_FIELDS = [:identifier, :title]
-    attr_accessor :fields
+    # Identity fields can only have a single value, everything else 
+    # can have multiple values
+    IDENTITY_FIELDS = [:identifier, :title, :creator, :publisher, :type]
+    attr_reader :fields
 
     def initialize(fields = {})
-      required_fields_present = []
       @fields = {}
       fields.each do |key, value|
-        required_fields_present << key.to_sym if REQUIRED_FIELDS.include?(key.to_sym)
-        field = Field.new(name: key, value: value)
-        @fields[field.name] = field
+        @fields[key.to_sym] = IDENTITY_FIELDS.include?(key.to_sym) ? 
+          new_field(key, value) : [new_field(key, value)].flatten
       end
-      required_fields_missing = REQUIRED_FIELDS - required_fields_present
-      unless required_fields_missing.empty?
-        raise ArgumentError.new("Missing required fields: #{required_fields_missing}") 
+      # Check for missing required fiexlds
+      raise_error_if_missing_required_fields
+      # Check for ambiguous "identity" fields
+      raise_error_if_ambiguous_identity_fields
+    end
+
+    def raise_error_if_missing_required_fields
+      missing_required_fields = (REQUIRED_FIELDS - @fields.keys)
+      unless missing_required_fields.empty?
+        raise ArgumentError.new("Missing required fields: #{missing_required_fields}")
       end
     end
+    private :raise_error_if_missing_required_fields
+
+    def raise_error_if_ambiguous_identity_fields
+      ambiguous_identity_fields =
+        IDENTITY_FIELDS.select{ |key| @fields[key].is_a? Array }
+      unless ambiguous_identity_fields.empty?
+        raise ArgumentError.new("Ambiguous identity fields: #{ambiguous_identity_fields}")
+      end
+    end
+    private :raise_error_if_ambiguous_identity_fields
+
+    def new_field(key, value)
+      if value.is_a? Array
+        value.map { |new_value| new_field(key, new_value) }
+      else
+        Field.new(name: key, value: value)
+      end
+    end
+    private :new_field
   end
 end
--- spec/nyucore/record_spec.rb
+++ spec/nyucore/record_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 module NyuCore
   describe Record do
-    let(:fields) do
-      { 
-        "identifier" => 1234567890,
-        "title" => "NYUCore/NyuCore",
-        subject: "NYUCore vs. NyuCore"
-      }
-    end
     context "when the required fields are present" do
+      let(:identity_fields) { Record::IDENTITY_FIELDS }
+      let(:fields) do
+        { "identifier" => 1234567890,
+          "title" => "NYUCore/NyuCore",
+          subject: "NYUCore vs. NyuCore" }
+      end
       subject(:record) { Record.new(fields) }
       it { should be_a(Record) }
-
+      it("should have identity fields") do
+        expect(identity_fields).not_to be_empty
+      end
       describe '#fields' do
         subject { record.fields }
         it { should be_a(Hash) }
@@ -20,20 +21,57 @@ module NyuCore
             expect(key).to be_a(Symbol)
           end
         end
-        it("should have values that are NyuCore::Fields") do
-          subject.each_value do |field|
-            expect(field).to be_a(Field)
+        it("should have single value fields that are NyuCore::Fields") do
+          subject.each do |key, value|
+            if identity_fields.include?(key)
+              expect(value).to be_a(Field)
+            end
           end
         end
+        context "when the multivalue fields have single values" do
+          it("should have multivalue fields that are Arrays containing 1 NyuCore::Field") do
+            subject.each do |key, value|
+              unless identity_fields.include?(key)
+                expect(value).to be_an(Array)
+                expect(value.size).to be(1)
+                value.each { |field| expect(field).to be_a(Field) }
+              end
+            end
+          end
+        end
+        context "when the multivalue fields have an Array of values" do
+          let(:fields) do
+            { "identifier" => 1234567890,
+              "title" => "NYUCore/NyuCore",
+              subject: ["NYUCore vs. NyuCore", "NYUCore's internal conflict"] }
+          end
+          it("should have multivalue fields that are Arrays containing multiple NyuCore::Fields") do
+            subject.each do |key, value|
+              unless identity_fields.include?(key)
+                expect(value).to be_an(Array)
+                expect(value.size).to be > 1
+                value.each { |field| expect(field).to be_a(Field) }
+              end
+            end
+          end
+        end
+      end
+    end
+    context "when a single value is passed in as an Array" do
+      let(:invalid_fields) do
+        { "identifier" => [1234567890, "0123456789"],
+          "title" => "NYUCore/NyuCore",
+          subject: "NYUCore vs. NyuCore" }
+      end
+      it("should raise an Argument Error") do
+        expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
       end
     end
     context "when some required fields are missing" do
       context "and it's the identifier that's missing" do
         let(:invalid_fields) do
-          { 
-            "title" => "NYUCore/NyuCore",
-            subject: "NYUCore vs. NyuCore"
-          }
+          { "title" => "NYUCore/NyuCore",
+            subject: "NYUCore vs. NyuCore" }
         end
         it("should raise an ArgumentError") do
           expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
@@ -41,10 +79,8 @@ module NyuCore
       end
       context "and it's the title that's missing" do
         let(:invalid_fields) do
-          { 
-            "identifier" => 1234567890,
-            subject: "NYUCore vs. NyuCore"
-          }
+          { "identifier" => 1234567890,
+            subject: "NYUCore vs. NyuCore" }
         end
         it("should raise an ArgumentError") do
           expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
@@ -52,9 +88,7 @@ module NyuCore
       end
       context "and it's both the idenitifier and title that are missing" do
         let(:invalid_fields) do
-          { 
-            subject: "NYUCore vs. NyuCore"
-          }
+          { subject: "NYUCore vs. NyuCore" }
         end
         it("should raise an ArgumentError") do
           expect{ Record.new(invalid_fields) }.to raise_error(ArgumentError)
